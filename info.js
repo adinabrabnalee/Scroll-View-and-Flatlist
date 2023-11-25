5d3005e1a456f51f39fa27dad70d7463cfedee62
adinabrabnalee <143054157+adinabrabnalee@users.noreply.github.com>
2023-11-25 22:06:56 +0800

    initial change

---
+++ info.js
@@ -0,0 +1,65 @@
+export const users = [
+    {
+      id:'01',
+      name: 'ANALIE V. BARBANIDA',
+      nickName:'Ann',
+      address:'Cabawan',
+      courseAndYear:'BSIT-3',
+      avatar:
+        'https://th.bing.com/th/id/R.9408bfbce3b0c6e302cdbf2c60538db8?rik=T2WOC2k0LjoRzA&riu=http%3a%2f%2fweneedfun.com%2fwp-content%2fuploads%2f2015%2f09%2fCute-baby-girl-pics-for-facebook-profile-13.jpg&ehk=OO8lsiG6DrOKTJ3NR6hTFsAZW1i3p3Jyt3uMxlNGuoA%3d&risl=&pid=ImgRaw&r=0',
+    },
+    {
+        id:'02',
+        name: 'PHOEVE VILLOREJO',
+        nickName:'BHE',
+        address:'Pangihan',
+        courseAndYear:'CRIM4',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'03',
+        name: 'JUNNY ALERT',
+        nickName:'JUN',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'04',
+        name: 'STANLY GWAPO',
+        nickName:'LEGENDARY',
+        address:'Poblacion',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'5',
+        name: 'PROFILE BEYOND',
+        nickName:'DARKGREY',
+        address:'Location',
+        courseAndYear:'BSIT 3A',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'6',
+        name: 'PAULINE DIMAIMO',
+        nickName:'kimmie',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },
+      {
+        id:'07',
+        name: 'PROFILE',
+        nickName:'NA',
+        address:'Location',
+        courseAndYear:'NA',
+        avatar:
+          'https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg',
+      },   
+  ];
